00cb9fd6d62d7971d61461bd5f920f1809e47981
lerakuzmina <l3ra131007@gmail.com>
2025-04-14 16:45:42 +0300

    done

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 from users.models import User
 
@@ -7,3 +8,9 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
